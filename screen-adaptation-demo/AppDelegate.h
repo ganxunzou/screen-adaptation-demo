4da38828ca9ae1f9b5c327329bea6ed0f2b9243f
ganxz <ganxz.sdc@sdc.com>
2018-12-05 09:38:29 +0800

    Initial Commit

---
+++ screen-adaptation-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  screen-adaptation-demo
+//
+//  Created by GXZ on 2018/12/5.
+//  Copyright © 2018 GXZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ screen-adaptation-demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  screen-adaptation-demo
+//
+//  Created by GXZ on 2018/12/5.
+//  Copyright © 2018 GXZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
