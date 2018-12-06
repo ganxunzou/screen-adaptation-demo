e59ac1aa2a6f17c1bc71b29fb23f6525b0e46651
GXZ <gxz@GXZdeiMac.local>
2018-12-06 15:26:21 +0800

    add webview

--- screen-adaptation-demo/WebViewController.m
+++ screen-adaptation-demo/WebViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "WebViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface WebViewController ()
+@property (weak, nonatomic) IBOutlet WKWebView *myWebView;
 
 @end
 
@@ -17,9 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+//    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"" ofType:nil]];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [_myWebView loadRequest:request];
+     _myWebView.backgroundColor = [UIColor blueColor];
+    [_myWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://127.0.0.1:8848/TestApp/index.html"]]]];
+    
+   
 }
 
-/*
+/*/Users/gxz/workspace_ios/BOCOMAPP/BOCOMAPP/Class/controllers/HomeViewController.m
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
