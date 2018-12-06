7b04a3e83a610830eaae1e9df7e57838b335df9c
GXZ <gxz@GXZdeiMac.local>
2018-12-06 13:38:07 +0800

    add webview

---
+++ screen-adaptation-demo/WebViewController.h
@@ -0,0 +1,17 @@
+//
+//  WebViewController.h
+//  screen-adaptation-demo
+//
+//  Created by  GXZ on 2018/12/6.
+//  Copyright © 2018 GXZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
